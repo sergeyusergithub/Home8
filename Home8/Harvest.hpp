16fb9135dd2c84282f181138d936840349d6c57b
Sergey Sergey <sergeymailnew@mail.ru>
2024-02-21 23:42:12 +0500

    Проект разбит на файлы.

---
+++ Home8/Harvest.hpp
@@ -0,0 +1,39 @@
+﻿// Harvest.hpp
+
+
+#pragma once
+
+#ifndef HARVEST_HPP
+#define HARVEST_HPP
+
+#include<iostream>
+
+// класс плодов
+class Harvest {
+public:
+	std::string Harvest_name() {
+		return harvest_name_;
+	};
+	void Harvest_name(std::string name) {
+		harvest_name_ = name;
+	};
+
+
+private:
+	std::string harvest_name_; // название плода
+	float harvest_weight_;	// вес плода
+	std::string harvest_color_; // цвет плода
+
+};
+
+class Apple :public Harvest {
+public:
+	Apple() {
+		Harvest_name("Apple");
+	}
+};
+
+
+#endif // !HARVEST_HPP
+
+
--- Home8/main.cpp
+++ Home8/main.cpp
@@ -1,56 +1,12 @@
-// main.cpp
+п»ї// main.cpp
 
-#include <iostream>
-
-
-// класс плодов
-class Harvest {
-public:
-	std::string Harvest_name() {
-		return harvest_name_;
-	};
-	void Harvest_name(std::string name) {
-		harvest_name_ = name;
-	};
+#include "Plants.hpp"
 
 
-private:
-	std::string harvest_name_; // название плода
-	float harvest_weight_;	// вес плода
-	std::string harvest_color_; // цвет плода
-
-};
-
-class Apple :public Harvest {
-public:
-	Apple() {
-		Harvest_name("Apple");
-	}
-};
 
 
-// класс растений
-class Plants {
-public:
-	virtual Harvest* CreateHarvest() = 0;
-};
 
-// класс яблоки
 
-class AppleTree :public Plants {
-public:
-	AppleTree() {
-
-	}
-	Harvest* CreateHarvest() override {
-		return new Apple;
-	}
-
-private:
-	std::string scale_harvest_; // размер плода (большой, маленький, средний)
-	std::string color_; // цвет
-	int number_harves_; // количество плодов 
-};
 
 
 
